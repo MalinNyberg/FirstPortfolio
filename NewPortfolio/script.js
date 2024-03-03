73483913c7c320229509ac4612451db81b07ce3d
Malin Nyberg <146171189+MalinNyberg@users.noreply.github.com>
2024-03-03 21:40:42 +0100

    Update script.js
    did a change in the code to make it work

--- NewPortfolio/script.js
+++ NewPortfolio/script.js
@@ -6,29 +6,6 @@ function toggleMenu(){
     sidebar.classList.toggle('active');
 }
 
-/*easter egg popup modal*/
-document.getElementById('button').addEventListener('click', function(){
-
-    document.querySelector('.llamaModal').style.display = 'flex';
-
-});
-
-document.querySelector('.close').addEventListener('click', function(){
-    
-    document.querySelector('.llamaModal').style.display ='none';
-});
-
-/*easter egg that changes color on background*/
-let result = document.getElementById("imgbox");
- 
-        function changeColor(color) {
-            document.body.style.background = color;
-        }
- 
-        function myFunc() {
-            changeColor('green');
-        }
-
 /*Easter egg with a modal that pops-up if the user presses some of the combinations below*/
 let pressedKeys = '';
 
@@ -53,3 +30,30 @@ function checkKeys(keys){
 function popUpAlert(message){
     alert(message);
 }
+
+
+
+/*easter egg popup modal*/
+document.getElementById('button').addEventListener('click', function(){
+
+    document.querySelector('.llamaModal').style.display = 'flex';
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+    
+    document.querySelector('.llamaModal').style.display ='none';
+});
+
+/*easter egg that changes color on background*/
+let result = document.getElementById("imgbox");
+ 
+        function changeColor(color) {
+            document.body.style.background = color;
+        }
+ 
+        function myFunc() {
+            changeColor('green');
+        }
+
+
