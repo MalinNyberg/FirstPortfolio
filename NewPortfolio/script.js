49c38dfa8afd73f9d9002f91c1406e46d2dc7beb
Malin Nyberg <146171189+MalinNyberg@users.noreply.github.com>
2024-03-03 21:23:33 +0100

    Update script.js
    updated with one more easter egg

--- NewPortfolio/script.js
+++ NewPortfolio/script.js
@@ -28,3 +28,28 @@ let result = document.getElementById("imgbox");
         function myFunc() {
             changeColor('green');
         }
+
+/*Easter egg with a modal that pops-up if the user presses some of the combinations below*/
+let pressedKeys = '';
+
+window.addEventListener('keyup', (e) => {
+    pressedKeys += e.key;
+
+    checkKeys(pressedKeys);
+
+    console.log(pressedKeys);
+});
+
+function checkKeys(keys){
+    if(keys === "1337") {
+        popUpAlert("Amigo, du är Eliiiit");
+    }
+    else if (keys === "net23"){
+        popUpAlert("The best class in the entire world");
+    }
+    
+}
+
+function popUpAlert(message){
+    alert(message);
+}
